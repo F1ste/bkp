97333e15023af4a3d5e7499397d659fef3204c30
Ruslan Guskov <feniksuh@gmail.com>
2023-10-06 17:32:15 +0300

    feedback status update

--- routes/user.php
+++ routes/user.php
@@ -48,7 +48,9 @@ Route::prefix('/profile')->middleware('auth')->group(function () {
     });
     Route::prefix('/feedback')->group(function(){
         Route::post('/',[FeedbackController::class,'store'])->name('profile.feedback.create');
-        Route::put('/',[FeedbackController::class,'update'])->name('profile.feedback.update');
+        Route::put('/accept',[FeedbackController::class,'accept'])->name('profile.feedback.accept');
+        Route::put('/decline',[FeedbackController::class,'decline'])->name('profile.feedback.decline');
+        Route::put('/viewed',[FeedbackController::class,'viewed'])->name('profile.feedback.viewed');
         Route::get('/candidat-{id}',[FeedbackController::class,'candidat_index'])->name('profile.feedback.candidat');
         Route::get('/owner-{id}',[FeedbackController::class,'owner_index'])->name('profile.feedback.owner');
         Route::get('/owner',[FeedbackController::class,'owner_all'])->name('profile.feedback.owner.all');
