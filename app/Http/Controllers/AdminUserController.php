404f05d822bd65a7801cdfe66d4bed515a9a3957
Ruslan Guskov <feniksuh@gmail.com>
2023-10-07 20:48:19 +0300

    rating fix

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -8,9 +8,16 @@ use Illuminate\Http\Request;
 
 class AdminUserController extends Controller
 {
-    public function index()
-    {
+    public function index(Request $request)
+    {   if($request->has('created_at')){
+        $user = User::orderBy('created_at','desc')->paginate(8);
+    }
+    elseif($request->has('rating')){
+        $user = User::orderBy('rating','desc')->paginate(8);
+    }
+    else{
         $user = User::paginate(8);
+    }
         return view('pages.admin.user.index')->with('user', $user);
     }
     public function edit($id)
