ca06e2c0bc3bce0b5c67df6ec0d50dfb8058439a
Fiste <f1ste@yandex.ru>
2023-10-07 19:01:12 +0300

    add and activate bvi plugin

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="{{ asset('front/css/style.css') }}"/>
+        <link rel="stylesheet" href="{{ asset('plugins/bvi/dist/css/bvi.min.css') }}"/>
         <title>@yield('title') - БКП</title>
     </head>
     <body>
@@ -18,5 +19,14 @@
         </div>
         <script src="{{ asset('front/js/app.js') }}"></script>
         <script src="{{ asset('front/js/app2.js') }}"></script>
+        <script src="{{ asset('plugins/bvi/dist/js/bvi.min.js') }}"></script>
+        <script>
+        new isvek.Bvi({
+            target: '.impaired-togler',
+            fontSize: 24,
+            theme: 'white'
+            //...etc
+        });
+        </script>
     </body>
 </html>
