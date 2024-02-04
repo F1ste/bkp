414889c7ff42771f5999eae1df5af54b44d3300e
Mikhail Lisovskii <f1ste@yandex.ru>
2024-02-04 21:51:39 +0300

    task 15

---
+++ app/Http/Controllers/PolicyController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class PolicyController extends Controller
+{
+    public function index()
+    {
+        return view('pages.policy');
+    }
+       
+}
