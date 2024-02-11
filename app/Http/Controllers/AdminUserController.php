1aa1ad0ca9582ed712ef2f751d634c6ca47ca81f
Valeriy Grechukha <v.mrfriz@gmail.com>
2024-02-11 15:08:10 +0300

    refactor: :recycle: AdminUserController

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -11,26 +11,26 @@ class AdminUserController extends Controller
     public function index(Request $request)
     {
         $sort_by = $request->input('sort_by', '');
+
+        $query = User::query();
         if ($sort_by === 'created_at') {
-            $user = User::orderBy('created_at', 'desc')->paginate(8);
+            $query = $query->orderBy('created_at', 'desc');
         } elseif ($sort_by === 'rating') {
-            $user = User::orderBy('rating', 'desc')->paginate(8);
-        } else {
-            $user = User::paginate(8);
+            $query = $query->orderBy('rating', 'desc');
         }
+        $user = $query->paginate(8);
 
         return view('pages.admin.user.index')->with('user', $user);
     }
 
     public function edit($id)
     {
-        $user = User::where('id', $id)->first();
+        $user = User::find($id);
         $user_rating = User::RATING;
         return view('pages.admin.user.edit', [
             'user' => $user,
             'id' => $id,
             'user_rating' => $user_rating
-
         ]);
     }
 
