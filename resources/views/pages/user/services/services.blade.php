1f51264fd635e3f78ef170a95be2733966439036
Valeriy Grechukha <v.mrfriz@gmail.com>
2024-02-11 20:30:49 +0300

    style(templates): :art: format pages/user templates

--- resources/views/pages/user/services/services.blade.php
+++ resources/views/pages/user/services/services.blade.php
@@ -4,48 +4,46 @@
 
 @section('content')
 
+    <div class="page__container">
+        <section class="my-projects personal-account">
+            <div class="my-projects__container">
+                <div class="my-projects__content">
+                    <div class="my-projects__heading block-heading">
+                        <div class="my-projects__title personal__title">
+                            Мои проекты
+                        </div>
+                    </div>
 
-			<div class="page__container">
-				<section class="my-projects personal-account">
-					<div class="my-projects__container">
-						<div class="my-projects__content">
-							<div class="my-projects__heading block-heading">
-								<div class="my-projects__title personal__title">
-									Мои проекты
-								</div>
-							</div>
+                    <div class="my-projects__projects-items">
+                        @foreach ($collections as $el)
+                            <div class="my-projects__item">
+                                <a href="{{ route('profile.services.single', ['id' => $el->id]) }}">
+                                    <div class="my-projects__project-name">
+                                        {{ $el->name_proj }}
+                                    </div>
+                                    <div class="my-projects__notifications _icon-notification">
+                                    @php
+                                        $counter = 0;
+                                    @endphp
+                                    @foreach ($notifications as $elevent)
+                                        @if ($elevent->id_project == $el->id)
+                                            @php $counter++;  @endphp
+                                        @endif
+                                    @endforeach
+                                    @php
+                                        echo $counter;
+                                    @endphp
+                                    </div>
+                                </a>
+                            </div>
+                        @endforeach
+                    </div>
 
+                    <a href="{{ route('profile.services.new') }}" class="my-projects__btn btn btn-white">Создать проект</a>
+                </div>
+            </div>
+        </section>
 
-							<div class="my-projects__projects-items">
-								@foreach($collections as $el)
-								<div class="my-projects__item">
-									<a href="{{ route('profile.services.single', ['id' => $el->id]) }}">
-										<div class="my-projects__project-name">
-											{{ $el->name_proj }}
-										</div>
-										<div class="my-projects__notifications _icon-notification">
-											@php
-											    $counter = 0;
-											@endphp
-											@foreach($notifications as $elevent)
-												@if($elevent->id_project == $el->id) @php $counter++;  @endphp @endif
-											@endforeach
-											@php
-											    echo($counter);
-											@endphp
-										</div>
-									</a>
-								</div>
-								@endforeach
-							</div>
-
-
-							<a href="{{ route('profile.services.new') }}" class="my-projects__btn btn btn-white">Создать проект</a>
-						</div>
-					</div>
-				</section>
-				
-			</div>
-
+    </div>
 
 @endsection
