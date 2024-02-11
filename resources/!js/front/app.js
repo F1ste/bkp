38b6736bb4072f5d9e82ab94726e9903e09e1fa3
Valeriy Grechukha <v.mrfriz@gmail.com>
2024-02-11 13:14:03 +0300

    refactor: :coffin: Удален resources/!js - не используется

--- resources/!js/front/app.js
+++
@@ -1,9 +0,0 @@
-import "./utils/menu"
-import "./utils/slider"
-import "./utils/phone-mask"
-import "./utils/logout"
-
-import "./setting/setting"
-
-import "./collection/collection-store"
-import "./collection/collection-edit"
